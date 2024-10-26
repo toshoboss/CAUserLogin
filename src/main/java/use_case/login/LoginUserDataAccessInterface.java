8fbc665f122abe5c412a8c527c5cccc76fc71e97
Dimitar <dimitar.toshev@mail.utoronto.ca>
2024-10-26 15:54:27 -0400

    task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the user with the given username.
+     * @param name the username to look up
+     */
+    void setCurrentUser(String name);
 }
